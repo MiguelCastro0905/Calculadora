3e947cd764c571a7c99b86ed091be16fa7cdefd0
Aprendiz <Aprendiz@BOGDFPCGMF4143.FormacionSena.RED>
2025-05-06 10:47:59 -0500

    Funcion-B mutiplicacion, division, potencia y divisionEntera

--- calculadora.js
+++ calculadora.js
@@ -1,45 +1,53 @@
-
 const readline = require("readline");
 const {
-  sumar,
-  restar,
+  multiplicar,
+  dividir,
+  potencia,
+  divisionEntera
 } = require("./operaciones");
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  function preguntar(mensaje) {
-    return new Promise((resolve) => rl.question(mensaje, resolve));
-  }
+function preguntar(mensaje) {
+  return new Promise((resolve) => rl.question(mensaje, resolve));
+}
 
-  async function main() {
-    try {
-      const num1 = await preguntar("Ingrese el primer número: ");
-      const num2 = await preguntar("Ingrese el segundo número: ");
-      const operador = await preguntar("Ingrese el operador (+, -): ");
-  
-      const a = parseFloat(num1);
-      const b = parseFloat(num2);
+async function main() {
+  try {
+    const num1 = await preguntar("Ingrese el primer número: ");
+    const num2 = await preguntar("Ingrese el segundo número: ");
+    const operador = await preguntar("Ingrese el operador ( /, *, **, //): ");
+
+    const a = parseFloat(num1);
+    const b = parseFloat(num2);
+
     if (isNaN(a) || isNaN(b)) {
-        throw new Error("Ambas entradas deben ser números.");
+      throw new Error("Ambas entradas deben ser números.");
     }
 
     let resultado;
 
-    if (operador === "+") {
-      resultado = sumar(a, b);
-    } else if (operador === "-") {
-      resultado = restar(a, b);
+    if (operador === "*") {
+      resultado = multiplicar(a, b);
+    } else if (operador === "/") {
+      resultado = dividir(a, b);
+    } else if (operador === "**") {
+      resultado = potencia(a, b);
+    } else if (operador === "//") {
+      resultado = divisionEntera(a, b);
     } else {
-        throw new Error("Operador inválido. Use uno de: +, -");
-      }
-      console.log(`Resultado: ${resultado}`);
-    } catch (error) {
-      console.error("Error:", error.message);
-    } finally {
-      rl.close();
+      throw new Error("Operador inválido. Use uno de: /, *, **, //");
     }
-    }  
-    main();
+
+    console.log(`Resultado: ${resultado}`);
+  } catch (error) {
+    console.error("Error:", error.message);
+  } finally {
+    rl.close();
+  }
+}
+
+main();
